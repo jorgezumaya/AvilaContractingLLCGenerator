48dc0917bce0be3035aded7091d35e316155a365
admin <admin@MacBookAir.lan>
2026-02-28 17:55:46 -0600

    added the date selector

--- AvilaContractingLLCGenerator/src/app/app.ts
+++ AvilaContractingLLCGenerator/src/app/app.ts
@@ -1,5 +1,7 @@
 import { Component, signal } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
 import { MatSlideToggleModule } from "@angular/material/slide-toggle";
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatInputModule} from '@angular/material/input';
@@ -10,15 +12,21 @@ export enum Status {
 }
 @Component({
   selector: "app-root",
-  imports: [RouterOutlet, MatSlideToggleModule, MatFormFieldModule, MatInputModule, MatDatepickerModule],
+  imports: [RouterOutlet, MatSlideToggleModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, FormsModule, CommonModule],
   templateUrl: "./app.html",
   styleUrl: "./app.css",
 })
 export class App {
   protected readonly title = signal("AvilaContractingLLCGenerator");
   EorI = signal(Status.E); //defaults to estimate (E), changes to invoice (I) when toggled
+  selectedDate = signal<Date | null>(null);
 
   toggleEorI() {
     this.EorI.set(this.EorI() === Status.E ? Status.I : Status.E);
   }
+
+  onDateChange(event: any) {
+    this.selectedDate.set(event);
+  }
+  
 }
