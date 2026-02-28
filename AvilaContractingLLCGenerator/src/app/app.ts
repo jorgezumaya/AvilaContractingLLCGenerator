3e813ea1467180090ca8f9da93e41de00ebb5312
admin <admin@MacBookAir.lan>
2026-02-28 15:20:40 -0600

    added EorI toggle for determining whether this is an invoice or an estimate

--- AvilaContractingLLCGenerator/src/app/app.ts
+++ AvilaContractingLLCGenerator/src/app/app.ts
@@ -1,12 +1,22 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
 
+export enum Status {
+  E = "Estimate",
+  I = "Invoice",
+}
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  selector: "app-root",
+  imports: [RouterOutlet, MatSlideToggleModule],
+  templateUrl: "./app.html",
+  styleUrl: "./app.css",
 })
 export class App {
-  protected readonly title = signal('AvilaContractingLLCGenerator');
+  protected readonly title = signal("AvilaContractingLLCGenerator");
+  EorI = signal(Status.E); //defaults to estimate (E), changes to invoice (I) when toggled
+
+  toggleEorI() {
+    this.EorI.set(this.EorI() === Status.E ? Status.I : Status.E);
+  }
 }
